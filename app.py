a0b896b3d85d477473f5e890010eb0d60e4fdc6b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-04-21 07:10:14 -0300

    4: Update greet function and add new function

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+fName = ""
+lName = ""
+
+def input_name():
+  global fName, lName
+  fName = input("Enter your first name: ")
+  lName = input("Enter your last name: ")
+
 def greet():
-  print("Hi there!")
+  input_name()
+  print(f"Hi there! {fName} {lName}")
 
 greet()
